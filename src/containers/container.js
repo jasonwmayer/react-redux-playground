8664759f8151b9ec92dcda622666f62a5d24694c
wley3337 <wley3337@gmail.com>
2019-05-07 09:44:32 -0400

    added test components and containers

---
+++ src/containers/container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Component from '../components/component';
+
+const Container = () =>{
+    return(
+        <div>Container
+            <Component />
+        </div>
+    )
+}
+export default Container
